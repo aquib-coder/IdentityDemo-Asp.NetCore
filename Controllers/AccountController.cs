035d55848e2456bd9fee39a02091c7d0375f93ee
Aquib Javed <atif9087@gmail.com>
2022-04-27 23:26:49 +0530

    Registration added

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityDemo_Asp.NetCore.ViewModel;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,8 +10,28 @@ namespace IdentityDemo_Asp.NetCore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registerViewModel.Email, Email = registerViewModel.Email };
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+            }
             return View();
         }
     }
